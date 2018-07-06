9cbd0e6eebd85023c7fc095a7d11330d7cc3165a
Adrien Pessu <a.pessu@gmail.com>
2018-07-06 20:47:50 +0200

    chore(test) : set up junit

---
+++ src/test/kotlin/com/github/bttfkotlin/kotlincodelabktor/MainTest.kt
@@ -0,0 +1,13 @@
+package com.github.bttfkotlin.kotlincodelabktor
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun `Kotlin is great`() {
+        assertTrue(true)
+    }
+}
+
