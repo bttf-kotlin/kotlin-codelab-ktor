963d92d86a3137a8507335e47150921de926c06b
Adrien Pessu <a.pessu@gmail.com>
2018-07-21 14:46:24 +0200

    feat(step2) : add convert number to letter

--- src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
+++ src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
@@ -1,7 +1,6 @@
 package com.github.bttfkotlin.kotlincodelabktor
 
 import com.github.bttfkotlin.kotlincodelabktor.service.EventService
+import com.github.bttfkotlin.kotlincodelabktor.util.EventUtil
 
-fun main(args: Array<String>) {
-    EventService.getAllEvents().forEach { println(it) }
-}
+fun main(args: Array<String>) = EventUtil.prettier(EventService.getAllEvents()).forEach { println(it) }
--- src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/util/EventUtil.kt
+++ src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/util/EventUtil.kt
@@ -10,6 +10,8 @@ object EventUtil {
     )
 
     fun prettier(events: List<Event>): List<Event> {
-        return events
+        return events.filter { it.date.isNotEmpty()}.map {
+            it.copy(date = """${unitsString[it.date[0].toString().toInt()]} ${unitsString[it.date[1].toString().toInt()]} ${unitsString[it.date[2].toString().toInt()]} ${unitsString[it.date[3].toString().toInt()]}""")
+        }
     }
 }
