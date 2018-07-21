907a4883bdeee186d0325cb9c301e893a63d8e70
Adrien Pessu <a.pessu@gmail.com>
2018-07-21 14:20:39 +0200

    fix(event) : remove useless white spaces

--- src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
+++ src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
@@ -2,6 +2,5 @@ package com.github.bttfkotlin.kotlincodelabktor
 
 import com.github.bttfkotlin.kotlincodelabktor.service.EventService
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) =
     EventService.getAllEvents().forEach { println(it) }
-}
