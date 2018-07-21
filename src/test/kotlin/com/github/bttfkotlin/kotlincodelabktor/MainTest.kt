1ac6b1d9ad9511be6ded02251f98cca9b14053ca
Adrien Pessu <a.pessu@gmail.com>
2018-07-21 14:20:39 +0200

    fix(step0): add description

--- src/test/kotlin/com/github/bttfkotlin/kotlincodelabktor/MainTest.kt
+++ src/test/kotlin/com/github/bttfkotlin/kotlincodelabktor/MainTest.kt
@@ -1,31 +1,13 @@
 package com.github.bttfkotlin.kotlincodelabktor
 
-import com.github.bttfkotlin.kotlincodelabktor.bean.Event
-import com.github.bttfkotlin.kotlincodelabktor.util.EventUtil
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class MainTest {
 
     @Test
-    fun `1985 should return one nine eight five`() {
-        val testedDate = "1985"
-
-        val events = listOf(Event(testedDate, "test", "test"))
-        val event = (EventUtil.prettier(events))[0]
-        assertEquals(
-                event.date
-                , "one nine eight five")
-    }
-
-    @Test
-    fun `When there is no date, nothing should be return`() {
-        val events = listOf(Event("", "test", "test"))
-        val size = EventUtil.prettier(events).size
-
-        assertEquals(
-                size
-                , 0)
+    fun `Kotlin is great`() {
+        assertTrue(true)
     }
 }
 
