3caf225ca913b5ea4aa3d63762da42ebd16252a8
Adrien Pessu <a.pessu@gmail.com>
2018-07-22 10:35:21 +0200

    feat(step3) : add ktor

--- src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
+++ src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
@@ -1,6 +1,40 @@
 package com.github.bttfkotlin.kotlincodelabktor
 
-import com.github.bttfkotlin.kotlincodelabktor.service.EventService
-import com.github.bttfkotlin.kotlincodelabktor.util.EventUtil
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.jackson.jackson
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
-fun main(args: Array<String>) = EventUtil.prettier(EventService.getAllEvents()).forEach { println(it) }
+fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(Compression)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+            setDefaultPrettyPrinter(DefaultPrettyPrinter().apply {
+                indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+                indentObjectsWith(DefaultIndenter("  ", "\n"))
+            })
+            registerModule(JavaTimeModule())  // support java.time.* types
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello JugSummer Camp")
+        }
+    }
+}
