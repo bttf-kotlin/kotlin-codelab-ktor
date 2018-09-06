d5dba280d0e07cfe209c15c8022230a6b84538bd
Adrien Pessu <a.pessu@gmail.com>
2018-09-06 13:29:52 +0200

    add missing UT

---
+++ src/test/kotlin/com/github/bttfkotlin/kotlincodelabktor/EventUtilText.kt
@@ -0,0 +1,31 @@
+package com.github.bttfkotlin.kotlincodelabktor
+
+import com.github.bttfkotlin.kotlincodelabktor.bean.Event
+import com.github.bttfkotlin.kotlincodelabktor.util.EventUtil
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class EventUtilText {
+
+    @Test
+    fun `1985 should return one nine eight five`() {
+        val testedDate = "1985"
+
+        val events = listOf(Event(testedDate, "test", "test"))
+        val event = (EventUtil.prettier(events))[0]
+        Assertions.assertEquals(
+                event.date
+                , "1985")
+    }
+
+    @Test
+    fun `When there is no date, nothing should be return`() {
+        val events = listOf(Event("", "test", "test"))
+        val size = EventUtil.prettier(events).size
+
+        Assertions.assertEquals(
+                size
+                , 0)
+    }
+}
+
