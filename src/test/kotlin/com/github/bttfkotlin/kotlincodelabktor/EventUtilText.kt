08a405724b020909ce6274fe3fd2c2aba2240f33
Adrien Pessu <a.pessu@gmail.com>
2018-10-13 11:18:46 +0200

    invert assert params

--- src/test/kotlin/com/github/bttfkotlin/kotlincodelabktor/EventUtilText.kt
+++ src/test/kotlin/com/github/bttfkotlin/kotlincodelabktor/EventUtilText.kt
@@ -1,6 +1,7 @@
 package com.github.bttfkotlin.kotlincodelabktor
 
 import com.github.bttfkotlin.kotlincodelabktor.bean.Event
+import com.github.bttfkotlin.kotlincodelabktor.util.EventUtil
 import com.github.bttfkotlin.kotlincodelabktor.util.prettier
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -8,15 +9,16 @@ import java.util.*
 
 class EventUtilText {
 
+
     @Test
     fun `1985 should return one nine eight five`() {
         val testedDate = "1985"
 
         val events = listOf(Event(testedDate, "test", "test"))
-        val event = (Date().prettier(events))[0]
+        val event = (EventUtil.prettier(events))[0]
         assertEquals(
-                event.date
-                , "one thousand nine hundred and eighty five")
+                "one thousand nine hundred and eighty five",
+                event.date)
     }
 
     @Test
@@ -24,20 +26,20 @@ class EventUtilText {
         val testedDate = "1913"
 
         val events = listOf(Event(testedDate, "test", "test"))
-        val event = (Date().prettier(events))[0]
+        val event = (EventUtil.prettier(events))[0]
         assertEquals(
-                event.date
-                , "one thousand nine hundred and thirteen")
+                "one thousand nine hundred and thirteen"
+                , event.date)
     }
 
     @Test
     fun `When there is no date, nothing should be return`() {
         val events = listOf(Event("", "test", "test"))
-        val size = Date().prettier(events).size
+        val size = EventUtil.prettier(events).size
 
         assertEquals(
-                size
-                , 0)
+                0
+                , size)
     }
 }
 
