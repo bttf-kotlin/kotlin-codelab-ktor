56279938feb633d1409994fc31473b379d321b5d
Adrien Pessu <a.pessu@gmail.com>
2018-10-19 13:53:43 +0200

    add devfest nantes

--- src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
+++ src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
@@ -39,7 +39,7 @@ fun Application.module() {
     }
     routing {
         get("/") {
-            call.respondText("Hello Devfest Nantes")
+            call.respondText("Hello devfest Nantes")
         }
         get("/events") {
             var events  = when (call.request.queryParameters["sort"]) {
