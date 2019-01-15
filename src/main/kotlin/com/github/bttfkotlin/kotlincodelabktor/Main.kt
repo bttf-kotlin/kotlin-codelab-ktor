59f21212ca63db20b1c7f64af766cecc73a57894
Adrien Pessu <adrien@pessu.net>
2019-01-15 10:34:59 +0100

    update ktor version

--- src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
+++ src/main/kotlin/com/github/bttfkotlin/kotlincodelabktor/Main.kt
@@ -20,7 +20,7 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import java.util.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     install(DefaultHeaders)
